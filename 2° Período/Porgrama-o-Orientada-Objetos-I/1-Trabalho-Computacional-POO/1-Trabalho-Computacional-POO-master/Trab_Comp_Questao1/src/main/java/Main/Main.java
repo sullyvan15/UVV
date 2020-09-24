fb4641341bdad318f5448c846427ad591bd8bc8b
Sullyvan Marks <sullyvanmarks@gmail.com>
2020-09-24 15:54:00 -0300

    .

---
+++ 2° Período/Porgrama-o-Orientada-Objetos-I/1-Trabalho-Computacional-POO/1-Trabalho-Computacional-POO-master/Trab_Comp_Questao1/src/main/java/Main/Main.java
@@ -0,0 +1,15 @@
+
+package Main;
+/**
+ *
+ * @author Sullyvan
+ */
+public class Main {
+    public static void main(String[] args) {
+        
+            int x = 0;
+        
+            Municipio municipio = new Municipio();
+                municipio.hashMap(x);
+    }
+}

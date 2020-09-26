a0b931a75afe53fed6c1df7be56495b7f71c690d
Sullyvan Marks <sullyvanmarks@gmail.com>
2020-09-26 16:44:52 -0300

    OK

--- 4° Período/Programação de Computadores/lista 2/Exercício 7.py
+++ 4° Período/Programação de Computadores/lista 2/Exercício 7.py
@@ -2,13 +2,11 @@ print ('Os numeros impares multiplos de 3 e não multiplos de 5 no intervalo [9,
 
 soma = 0
 contagem = 0
-
-for contador in range (9 , 90):
+# Ok
+for contador in range(9, 90):
     if (contador % 2 == 1 and contador % 3 == 0 and contador % 5!= 0):
         soma = contador + soma
         contagem = contagem = contagem + 1
 
-        print(f'soma: {soma}')
-        print(f'Contagem dos numeros: {contagem}')
 
-        
+        print(f'Soma N°{contagem}: {soma}')

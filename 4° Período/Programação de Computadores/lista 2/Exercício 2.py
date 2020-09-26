a61ebe8ed8d9440e67b1b59002bc93fb4cf467b0
Sullyvan Marks <sullyvanmarks@gmail.com>
2020-09-26 19:33:00 -0300

    OK (Código reformatado)

--- 4° Período/Programação de Computadores/lista 2/Exercício 2.py
+++ 4° Período/Programação de Computadores/lista 2/Exercício 2.py
@@ -1,12 +1,11 @@
 somatorio = 0
 med = 0
 cont = 0
-# Necessita de revisão
-for cont in range (200, 100, -1):
-    if(cont % 11 == 0):
-        
+
+for cont in range(200, 100, -1):
+    if cont % 11 == 0:
         print(f'Multiplo: {cont}')
         somatorio = cont + somatorio
         med = somatorio / cont
-    print(f'somatorio: {somatorio}')
-    print(f'med: {med}')
+print(f'somatorio: {somatorio}')
+print(f'med: {med}')
